9f2d3d358b1e641ce4faeea52a8e57f0e5c0c048
bogind <dror@kaplanopensource.co.il>
2022-03-24 21:42:13 +0200

    added info and reademe

---
+++ README.md
@@ -0,0 +1,6 @@
+## מפת חסימת רחובות עקב מרתון ירושלים 2022
+מפה של רחובות חסומים לפי המידע שפרסמה עיריית ירושלים ב[קישור הבא](https://www.jerusalem.muni.il/he/newsandarticles/traffic/jerusalem-marathon-traffic-2022/)  
+המרתון מתקיים ביום שישי ה25/03/2022 
+
+שכבת הרחובות נלקחה מ[ממאגר הGIS של העירייה](https://gisviewer.jerusalem.muni.il/arcgis/rest/services/BaseLayers/MapServer/27) כל האי דיוקים אם קיימים הם במקור.  
+החסימות סומנו אך ורק לפי שמות הרחובות שפורסמו ולא נעשתה בקרת איכות
--- js/index.js
+++ js/index.js
@@ -9,4 +9,33 @@ const map = L.map('map',{
     zoom: 13,
 });
 
+L.Control.Info = L.Control.extend({
+    onAdd: function(map) {
+        this._container = L.DomUtil.create('div', 'leaflet-bar');
+        this._container.style.backgroundColor = "white";
+        this._container.style.margin = "5px";
+        this._container.style.direction = "rtl";
+        this._container.style.width = "200px"
+        //this._container.style.whiteSpace =  "nowrap";
+        L.DomEvent.disableScrollPropagation(this._container)
+        L.DomEvent.disableClickPropagation(this._container)
+
+        this._container.innerHTML = "<p>מפת רחובות חסומים בירושלים לכבוד מרתון ווינר 2022\
+        המידע נלקח מ<a href='https://www.jerusalem.muni.il/he/newsandarticles/traffic/jerusalem-marathon-traffic-2022/'>הודעת המירוץ</a> ומ<a href='https://gisviewer.jerusalem.muni.il/arcgis/rest/services/BaseLayers/MapServer/27'>GIS העירייה</a></p>"
+
+        
+
+        return this._container;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+
+L.control.info = function(opts) {
+    return new L.Control.Info(opts);
+}
+L.control.info({ position: 'bottomleft' }).addTo(map);
+
 onMapLoad()
